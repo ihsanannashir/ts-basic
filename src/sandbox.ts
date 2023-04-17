df31eeddec582907184a6f908a0376fb7aae52f7
ihsanannashir <ihsanannashir@gmail.com>
2023-04-17 15:17:45 +0700

    set workflow and config

---
+++ src/sandbox.ts
@@ -0,0 +1,27 @@
+// const char = 'naruto';
+
+// console.log(char);
+
+// const inputs = document.querySelectorAll('input');
+
+// console.log(inputs);
+
+// inputs.forEach(input => {
+//     console.log(input);
+// });
+
+//tsc sandbox.ts for manual compiling
+//tsc sandbox.ts -w for auto compiling
+
+// const circ = (diameter: number) => {
+//     return diameter * Math.PI;
+// }
+
+// console.log(circ(7.5)); //it will works
+// console.log(circ('halo')) //it won't works (error)
+
+let mixed = [2,4,'anj'];
+
+mixed.push('halo');
+mixed.push(3);
+console.log(mixed);
