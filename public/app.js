5883ce11c28584a0d032e932d9014bc979fcdfd9
ihsanannashir <ihsanannashir@gmail.com>
2023-04-24 17:02:33 +0700

    interfaces

--- public/app.js
+++ public/app.js
@@ -2,6 +2,21 @@
 //tsc sandbox.ts -w for auto compiling
 //tsc -w for auto compiling all
 import { Invoice } from "./classes/Invoice.js";
+const me = {
+    name: 'muller',
+    age: 33,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,31 @@
 //tsc -w for auto compiling all
 import { Invoice } from "./classes/Invoice.js";
 
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'muller',
+    age: 33,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+}
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello', person.name);
+}
+
+greetPerson(me);
+
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
