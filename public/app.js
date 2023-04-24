5dc6a79cec82a413ff8f46f39419fc9837e5b065
ihsanannashir <ihsanannashir@gmail.com>
2023-04-24 16:04:53 +0700

    classes

--- public/app.js
+++ public/app.js
@@ -2,6 +2,25 @@
 //tsc sandbox.ts for manual compiling
 //tsc sandbox.ts -w for auto compiling
 //tsc -w for auto compiling all
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invoices);
+console.log(invOne, invTwo);
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,35 @@
 //tsc sandbox.ts -w for auto compiling
 //tsc -w for auto compiling all
 
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+
+console.log(invoices);
+console.log(invOne, invTwo);
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 // inputs
@@ -19,4 +48,4 @@ form.addEventListener('submit', (e: Event) => {
         details.value,
         amount.valueAsNumber
     );
-})
+});
